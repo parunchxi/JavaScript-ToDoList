d39a34f5fb3f62086592bc1bfbdc2fc7700ace2c
parunchxi <parunchxi@gmail.com>
2023-06-03 17:31:14 +0700

    Add Task System

---
+++ script.js
@@ -0,0 +1,18 @@
+const inputTask = document.querySelector('#new_task');
+const addTaskButton = document.querySelector('#add');
+const taskList = document.querySelector('ul');
+
+inputTask.addEventListener('keydown', () => {
+    if (inputTask.value !== '') {
+        addTaskButton.disabled = false;
+    } else {
+        addTaskButton.disabled = true;
+    }
+})
+
+addTaskButton.addEventListener('click', () => {
+    let li = document.createElement('li');
+    li.innerHTML = inputTask.value;
+    taskList.appendChild(li);
+    inputTask.value = '';
+})
