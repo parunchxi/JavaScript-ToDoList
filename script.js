266f03ff2d9f4c1159686b00826bbf076eeb5398
parunchxi <parunchxi@gmail.com>
2023-06-07 00:21:20 +0700

    Add save theme system

--- script.js
+++ script.js
@@ -73,11 +73,37 @@ taskList.addEventListener('click', event => {
 
 // Change Theme
 const changeTheme = document.getElementById('change_theme');
+
 changeTheme.addEventListener('click', () => {
-    document.body.classList.toggle('dark-mode');
     if (document.body.className === 'dark-mode') {
-        changeTheme.innerHTML = '<i class="ri-moon-fill"></i>';
+        setLightMode();
     } else {
-        changeTheme.innerHTML = '<i class="ri-sun-fill"></i>';
+        setDarkMode();
     }
 })
+
+function setDarkMode() {
+    document.body.classList.add('dark-mode');
+    changeTheme.innerHTML = '<i class="ri-moon-fill"></i>';
+    saveTeme('darkMode');
+}
+
+function setLightMode() {
+    document.body.classList.remove('dark-mode');
+    changeTheme.innerHTML = '<i class="ri-sun-fill"></i>';
+    saveTeme('lightMode');
+}
+
+// Get Theme
+const theme = localStorage.getItem('theme');
+
+if (theme === 'darkMode') {
+    setDarkMode();
+} else if (theme === 'lightMode') {
+    setLightMode();
+}
+
+// Save Theme
+function saveTeme(theme) {
+    localStorage.setItem('theme', theme);
+}
